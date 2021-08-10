f6dcbe3df7505a7b30951471f33e0c104968984f
Alex Weber <weber.alexander1989@gmail.com>
2021-08-10 08:02:31 +0200

    video implementation

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # zeldaplayer
+
+This project is my personal project to get my grips with Python and OpenCV again. I work sporadically on this and post progress.
+
+The grand idea is to have an algorithm that can scan all information on the screen for ALttP and process it to play the game. The grand goal would be to have an AI that can solve an easy randomizer on its own, only using some learned knowledge (no SNES RAM access) and visuals. This will likely take years. 
--- zelda.py
+++ zelda.py
@@ -2,14 +2,17 @@ import cv2 as cv
 import numpy as np
 import time
 
-IMG_RGB = cv.imread('testimages/zelda5.png')
+#IMG_RGB = cv.imread('testimages/zelda5.png')
+
 
 def createNumberSlices():
   img_numbers = cv.imread('masks/numbers.png', 0)
   return [img_numbers[0:7,int(i*7):int(i*7)+7] for i in range(0,10)]
 
-def findHearts(threshold):
-  img_gray = cv.cvtColor(IMG_RGB, cv.COLOR_BGR2GRAY)
+NUMBERS = createNumberSlices()
+
+def findHearts(frame, threshold):
+  img_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
   template = cv.imread('masks/heart_full.png',0)
   mask = cv.imread('masks/heart_mask.png',cv.IMREAD_GRAYSCALE)
   w, h = template.shape[::-1]
@@ -23,8 +26,8 @@ def findHearts(threshold):
       full_h+=1
   return full_h,all_h
 
-def findHalfHeart(threshold):
-  img_gray = cv.cvtColor(IMG_RGB, cv.COLOR_BGR2GRAY)
+def findHalfHeart(frame, threshold):
+  img_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
   template = cv.imread('masks/heart_half.png',0)
   mask = cv.imread('masks/heart_full_mask.png',cv.IMREAD_GRAYSCALE)
   y, x = img_gray.shape[::]
@@ -35,10 +38,11 @@ def findHalfHeart(threshold):
 def findItemCount(img_slice):
   loc_array = np.empty(10, dtype=object)
   loc_list = []
-  tr, bw_slice = cv.threshold(img_slice,240,255,cv.THRESH_BINARY)
+  tr, bw_slice = cv.threshold(img_slice,200,255,cv.THRESH_BINARY)
+  cv.imshow('items',bw_slice)
   cv.imwrite('masks/items.png',bw_slice)
   for i in range(0,10):
-    loc_array = np.where( cv.matchTemplate(bw_slice,NUMBERS[i],cv.TM_CCOEFF_NORMED,NUMBERS[i]) >= .92)
+    loc_array = np.where( cv.matchTemplate(bw_slice,NUMBERS[i],cv.TM_CCOEFF_NORMED,NUMBERS[i]) >= .95)
     for pt in zip(*(loc_array)[::-1]):
       loc_list.append([pt[0],pt[1],i])
   loc_list.sort(key=lambda loc:loc[0])
@@ -64,31 +68,55 @@ def findItemCount(img_slice):
     i+=1
   return rupee, bomb, arrow, key
 
-def getMagicMeter():
-  ret, thres = cv.threshold(cv.cvtColor(IMG_RGB, cv.COLOR_BGR2GRAY)[23:55,25:26],20,255,cv.THRESH_BINARY)
+def getMagicMeter(frame):
+  ret, thres = cv.threshold(cv.cvtColor(frame, cv.COLOR_BGR2GRAY)[23:55,25:26],20,255,cv.THRESH_BINARY)
   return int((cv.countNonZero(thres)/thres.size)*10000)/100
 
 
+frames = []
+cap = cv.VideoCapture('testimages/vid1.mp4')
 
-NUMBERS = createNumberSlices()
+# Check if camera opened successfully
+if (cap.isOpened()== False): 
+  print("Error opening video stream or file")
 
-createNumberSlices()
-start_time=time.monotonic()
-fh, ah = findHearts(.8)
-magic = getMagicMeter()
-hh = findHalfHeart(.8)
-#cv.rectangle(IMG_RGB,(60,20),(160,35),(0,0,255),1)
-rupee,bomb,arrow,key =findItemCount(cv.cvtColor(IMG_RGB, cv.COLOR_BGR2GRAY)[20:35,60:160])
-end_time=time.monotonic()
-if (hh):
-  fh+=.5
-font = cv.FONT_HERSHEY_SIMPLEX
-cv.putText(IMG_RGB,'HP   : '+str(fh)+'/'+str(ah),(180,50), font, 0.3,(255,255,255),1,cv.LINE_AA)
-cv.putText(IMG_RGB,'Rupee: '+str(rupee),(180,65), font, 0.3,(255,255,255),1,cv.LINE_AA)
-cv.putText(IMG_RGB,'Bomb : '+str(bomb),(180,80), font, 0.3,(255,255,255),1,cv.LINE_AA)
-cv.putText(IMG_RGB,'Arrow: '+str(arrow),(180,95), font, 0.3,(255,255,255),1,cv.LINE_AA)
-cv.putText(IMG_RGB,'Key  : '+str(key),(180,110), font, 0.3,(255,255,255),1,cv.LINE_AA)
-cv.putText(IMG_RGB,'Magic: '+str(magic)+'%',(180,125), font, 0.3,(255,255,255),1,cv.LINE_AA)
+# Read until video is completed
+while(cap.isOpened()):
+  # Capture frame-by-frame
+  ret, frame = cap.read()
+  if ret == True:
+    start_time=time.monotonic()
+    fh, ah = findHearts(frame,.8)
+    magic = getMagicMeter(frame)
+    hh = findHalfHeart(frame,.8)
+    rupee,bomb,arrow,key =findItemCount(cv.cvtColor(frame, cv.COLOR_BGR2GRAY)[24:31,60:160])
+    if (hh):
+      fh+=.5
+    font = cv.FONT_HERSHEY_SIMPLEX
+    #cv.rectangle(frame,(60,20),(160,35),(0,255,0),3)
+    cv.putText(frame,'HP   : '+str(fh)+'/'+str(ah),(180,50), font, 0.3,(255,255,255),1,cv.LINE_AA)
+    cv.putText(frame,'Rupee: '+str(rupee),(180,65), font, 0.3,(255,255,255),1,cv.LINE_AA)
+    cv.putText(frame,'Bomb : '+str(bomb),(180,80), font, 0.3,(255,255,255),1,cv.LINE_AA)
+    cv.putText(frame,'Arrow: '+str(arrow),(180,95), font, 0.3,(255,255,255),1,cv.LINE_AA)
+    cv.putText(frame,'Key  : '+str(key),(180,110), font, 0.3,(255,255,255),1,cv.LINE_AA)
+    cv.putText(frame,'Magic: '+str(magic)+'%',(180,125), font, 0.3,(255,255,255),1,cv.LINE_AA)
+    end_time=time.monotonic()
+    # Display the resulting frame
+    cv.imshow('Frame',frame)
+    frames.append(1/(end_time-start_time))
+    # Press Q on keyboard to  exit
+    if cv.waitKey(1) & 0xFF == ord('q'):
+      break
 
-cv.imwrite('res.png',IMG_RGB)
-print(end_time-start_time)
+  # Break the loop
+  else: 
+    break
+
+# When everything done, release the video capture object
+cap.release()
+
+# Calculate average FPS
+print('Average FPS: ',(sum(frames)/len(frames)))
+
+# Closes all the frames
+cv.destroyAllWindows()
